085078fe2dc793ccb249fda1adccbcf839362ff9
AirSeeker <zimorget@gmail.com>
2016-09-10 13:26:01 +0300

    fix issue when alot commas after some item

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -12,12 +12,18 @@
 
 		$scope.checkList = function(){
 			var getArrayOfList = $scope.listOfNames.trim().split(',');
-
+			
+			for (var i = getArrayOfList.length - 1; i >= 0; i--) {
+				if(getArrayOfList[i] === ''){
+					getArrayOfList.splice(i, 1);
+				}
+			}
+			console.log(getArrayOfList.length);
 			if(getArrayOfList[getArrayOfList.length-1] === '' && getArrayOfList.length > 1){
 				getArrayOfList.pop();
 			}
 
-			if(getArrayOfList.length === 1 && getArrayOfList[0] === ''){
+			if(getArrayOfList.length === 0){
 				return $scope.message = 'Please enter data first';
 			}
 
